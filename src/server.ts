2359c1f6651c4e06fe4f3fac1dbb251d35ccec09
minhchi1509 <minhchico300kc@gmail.com>
2023-04-09 23:43:17 +0700

    add mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
 
 const app: Application = express();
-const port = 8080; // Khai báo cổng
+const port = 8080;
+
 dotenv.config();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World123456');
+(async () => {
+  await mongoose.connect(process.env.MONGODB_URL as string);
+  console.log('Successfully connected to MongoDB');
+})().catch(() => {
+  console.log('Failed to connect to MongoDB.');
 });
 
 app.listen(port, () => {

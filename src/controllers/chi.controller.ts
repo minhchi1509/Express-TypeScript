08a97de8a8114c32a2bbca03160023e7a26ccbb5
minhchi1509 <minhchico300kc@gmail.com>
2023-05-23 14:59:35 +0700

    delete

--- src/controllers/chi.controller.ts
+++ src/controllers/chi.controller.ts
@@ -1,10 +1,4 @@
 const chiController = () => {
   return 'onlyInMainBranch123';
 };
-
-const chiController2 = () => {
-  return 'chiController2';
-};
-
-export { chiController2 };
 export default chiController;

99aed1ad6dae800e3a5ec0f364e8bb319fa75a37
minhchi1509 <minhchico300kc@gmail.com>
2023-05-23 14:57:22 +0700

    add ChiController2

--- src/controllers/chi.controller.ts
+++ src/controllers/chi.controller.ts
@@ -2,4 +2,9 @@ const chiController = () => {
   return 'onlyInMainBranch123';
 };
 
+const chiController2 = () => {
+  return 'chiController2';
+};
+
+export { chiController2 };
 export default chiController;

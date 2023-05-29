a9187a5fd4918c43ddfcc35de8bb40ea358e7ab3
minhchi1509 <minhchico300kc@gmail.com>
2023-05-29 22:58:59 +0700

    abc

--- src/controllers/chi.controller.ts
+++ src/controllers/chi.controller.ts
@@ -1,4 +1,8 @@
 const wrongCodeOnBranchMain = () => {
   return 'wrongCodeOnBranchMain';
 };
+
+const fromDashboard = () => {
+  console.log('Hi, it is from dashboard want to merge to main');
+};
 export default wrongCodeOnBranchMain;

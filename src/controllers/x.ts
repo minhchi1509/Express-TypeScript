deb66019f3bb8d8e03ab8a1afa8c985377f9b678
minhchi1509 <minhchico300kc@gmail.com>
2023-05-30 00:08:47 +0700

    a

--- src/controllers/x.ts
+++ src/controllers/x.ts
@@ -1,3 +1,9 @@
-export const xMain = () => {
+const xMain = () => {
   console.log('xMain and dont change that. Remember that..........');
 };
+
+const yMain = () => {
+  console.log('yMain and dont change that. Remember that..........');
+};
+
+export { xMain, yMain };
--- src/controllers/y.ts
+++
@@ -1,3 +0,0 @@
-export const YMain = () => {
-  console.log('YMain and remember not to change that..........');
-};
